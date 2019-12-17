ef3eaf670b69ed5e41c9955aa96eb86e81464082
Sergio Martin de la Higuera <sergiomartindelahiguera@gmail.com>
2019-12-17 20:56:33 +0100

    Log in

--- Web Project/Controller.py
+++ Web Project/Controller.py
@@ -1,10 +1,13 @@
 import web   
-from Models import RegisterModel
+from Models import RegisterModel, LoginModel
 
 urls = (
     '/', 'home',
+    '/login', 'login',
     '/register', 'register',
-    '/postregistration', 'postRegistration'
+    '/postregistration', 'postRegistration',
+    '/check-login', 'checkLogin'
+    
 )
 
 TEMPLATE_PATH = './Views/Templates'
@@ -17,6 +20,22 @@ render = web.template.render(TEMPLATE_PATH, base=BASE_FILE)
 class home:
     def GET(self):
         return render.home()
+
+class login:
+    def GET(self):
+        return render.login()
+
+class checkLogin:
+    def POST(self):
+        data = web.input()
+
+        login_model = LoginModel.LoginModel()
+        isCorrect = login_model.check_user(data)
+
+        if isCorrect:
+            return isCorrect
+
+        return "error"
 
 class register:
     def GET(self):
--- Web Project/static/js/scripty.js
+++ Web Project/static/js/scripty.js
@@ -15,4 +15,21 @@ $(document).ready(function(){
             }
         });
     });
+
+    $(document).on("submit", "#login-form", function(e){
+        e.preventDefault();
+
+        var form = $('#login-form').serialize();
+        $.ajax({
+             url: '/check-login',
+             type: 'POST',
+             data: form,
+             success: function(res){
+                 if (res == "error") 
+                    alert("Could not log in.");
+                else 
+                    console.log("Logged in as ", res);
+             }
+        })
+    })
 });
