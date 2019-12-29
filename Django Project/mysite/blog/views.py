981bbb66f154c76f1e3169e1f335e2c14787b9b8
Sergio Martin de la Higuera <sergiomartindelahiguera@gmail.com>
2019-12-29 11:11:22 +0100

    Adding posts to index

--- Django Project/mysite/blog/views.py
+++ Django Project/mysite/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey there!")
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
 
 def post(request):
     return HttpResponse("I'm a single post page")
