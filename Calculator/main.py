586c15afc7683ab58a1ac40fc7282d399a8679bd
Sergio Martin de la Higuera <sergiomartindelahiguera@gmail.com>
2019-12-31 20:50:09 +0100

    button manipulation

--- Calculator/main.py
+++ Calculator/main.py
@@ -7,12 +7,15 @@ class MainWindow(QWidget):
         super().__init__() # This supper is related to QWidget
 
         self.init_ui()
+        self.counter = 0
 
     def init_ui(self):
         label = QLabel("Name: ")
         name_input = QLineEdit()
 
-        button = QPushButton("Set Name")
+        self.button = QPushButton("Clicked")
+        self.button.released.connect(self.releasedButton)
+        self.button.clicked.connect(self.clickedButton)
 
         h = QHBoxLayout()
 
@@ -22,13 +25,20 @@ class MainWindow(QWidget):
         v = QVBoxLayout()
         v.addLayout(h)
 
-        v.addWidget(button)
+        v.addWidget(self.button)
 
         self.setLayout(v)
         self.setWindowTitle("Horizontal inside Vertical Layaout")
 
         self.show()
 
+    def clickedButton(self):
+        print("This button has been clicked")
+
+    def releasedButton(self):
+        print("This button has been released")
+        self.counter += 1
+        self.button.setText("Clicked: {}".format(self.counter))
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
